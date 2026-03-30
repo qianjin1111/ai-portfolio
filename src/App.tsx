65b7f3442f73fb232e2c69291f02689902d78a9f
qianjin.eddie__dcar <qianjin.eddie@dcarlife.com>
2026-03-30 18:02:42 +0800

    Fix: Use HashRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import { Home, FolderOpen, BookOpen, User, Mail } from 'lucide-react';
 import HomePage from './pages/HomePage';
 import ProjectsPage from './pages/ProjectsPage';
